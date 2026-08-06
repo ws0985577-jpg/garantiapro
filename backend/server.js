6e5c5b53bee3156d66d6d9cfdbc12bcfda91bf5e
Welllington Silva <ws0985577@gmail.com>
2026-08-06 15:15:32 -0300

    pix mercado pago

--- backend/server.js
+++ backend/server.js
@@ -3,30 +3,23 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { MercadoPagoConfig, Preference, Payment } from "mercadopago";
 
-
 dotenv.config();
 
-
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
 
-
 const client = new MercadoPagoConfig({
-
     accessToken: process.env.MP_ACCESS_TOKEN
-
 });
 
 
 
-
+// CRIAR PAGAMENTO
 
 app.post("/criar-pagamento", async (req,res)=>{
-
 
 try{
 
@@ -37,41 +30,28 @@ const { plano } = req.body;
 let valor = 0;
 
 
-
 if(plano === "Mensal"){
-
     valor = 25.90;
-
 }
-
 
 
 if(plano === "Anual"){
-
     valor = 99.90;
-
 }
-
 
 
 
 if(valor === 0){
 
 return res.status(400).json({
-
 erro:"Plano inválido"
-
 });
 
 }
 
 
 
-
-
 const preference = new Preference(client);
-
-
 
 
 
@@ -98,24 +78,30 @@ unit_price:valor
 
 
 
-
 payment_methods:{
 
+
 excluded_payment_types:[
+
 {
 id:"ticket"
 },
+
 {
 id:"atm"
 }
+
 ],
 
+
 installments:12
+
 
 },
 
 
 
+// Pix precisa estar habilitado na conta Mercado Pago
 
 back_urls:{
 
@@ -127,12 +113,15 @@ failure:"https://garantiapro.com.br/admin/pagamento",
 pending:"https://garantiapro.com.br/admin/pagamento"
 
 
+},
+
+
+
+auto_return:"approved"
+
+
 }
 
-
-
-
-}
 
 
 });
@@ -157,7 +146,6 @@ url:pagamento.init_point
 console.log("Erro Mercado Pago:",error);
 
 
-
 res.status(500).json({
 
 erro:"Erro ao criar pagamento"
@@ -168,7 +156,6 @@ erro:"Erro ao criar pagamento"
 }
 
 
-
 });
 
 
@@ -177,16 +164,15 @@ erro:"Erro ao criar pagamento"
 
 
 
+// WEBHOOK
 
-// WEBHOOK MERCADO PAGO
-
-app.post("/webhook", async (req,res)=>{
+app.post("/webhook", async(req,res)=>{
 
 
 try{
 
 
-console.log("Webhook recebido:",req.body);
+console.log("Webhook:",req.body);
 
 
 
@@ -194,7 +180,6 @@ if(req.body.type === "payment"){
 
 
 const idPagamento = req.body.data.id;
-
 
 
 const payment = new Payment(client);
@@ -209,20 +194,18 @@ id:idPagamento
 
 
 
-console.log("Status pagamento:", resultado.status);
-
+console.log(
+"Status:",
+resultado.status
+);
 
 
 
 if(resultado.status === "approved"){
 
-
 console.log("Pagamento aprovado!");
 
-
-
 }
-
 
 
 }
@@ -236,8 +219,7 @@ res.sendStatus(200);
 }catch(error){
 
 
-console.log("Erro webhook:",error);
-
+console.log(error);
 
 res.sendStatus(500);
 
@@ -245,9 +227,7 @@ res.sendStatus(500);
 }
 
 
-
 });
-
 
 
 
@@ -257,9 +237,7 @@ res.sendStatus(500);
 
 app.get("/",(req,res)=>{
 
-
 res.send("GarantiaPro API Online");
-
 
 });
 
@@ -268,12 +246,13 @@ res.send("GarantiaPro API Online");
 
 
 
+const PORT = process.env.PORT || 3000;
 
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
 
-
-console.log("Mercado Pago rodando na porta 3000");
-
+console.log(
+`Mercado Pago rodando na porta ${PORT}`
+);
 
 });
